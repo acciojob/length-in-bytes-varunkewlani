c8a1dc8c4d4a82ca89ad9a98f8e81f8187388d1e
varunkewlani <91694985+varunkewlani@users.noreply.github.com>
2024-07-22 21:22:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const byteSize = (str) => {
+	return (str.length)
   // write your code here
 };
 
 // Do not change the code below
-const str = prompt("Enter some string.");
-alert(byteSize(str));
+const str = prompt("Enter some string.")
+alert(byteSize(str))
